2dacdea7f5b916fc1feec0a743da1d3f12c17ac7
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2020-10-19 14:48:27 +0530

    Update Green.py

--- Basic Projects/Colour-Detection/Green.py
+++ Basic Projects/Colour-Detection/Green.py
@@ -1,31 +1,35 @@
+
+import numpy as np
 import cv2
 
-# Load image, grayscale, Gaussian blur, adaptive threshold
-image = cv2.imread("image.png")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray, (5,5), 0)
-thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,15,30)
+cap = cv2.VideoCapture(0)
 
-# Dilate to combine adjacent text contours
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
-dilate = cv2.dilate(thresh, kernel, iterations=3)
+while(True):
+    ret, img = cap.read()
+    hsv=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
+    
+    lower_green = np.array([28,52,72])
+    upper_green = np.array([87,255,255])
+    mask = cv2.inRange(hsv , lower_green , upper_green)
+    
+     
+    
+    blur=cv2.GaussianBlur(mask,(5,5),0)
+    
+    
+    contours, hierarchy = cv2.findContours(blur,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    
+    for cnt in contours:
+        area=0
+        area=cv2.contourArea(cnt)
+        if area>500:
+            x,y,m,h=cv2.boundingRect(cnt)
+            img= cv2.rectangle(img,(x,y),(x+m,y+h),(255,0,0),2)
+            
+            
+    cv2.imshow("frame",img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      break     
 
-# Find contours, highlight text areas, and extract ROIs
-cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if len(cnts) == 2 else cnts[2]
-
-ROI_number = 0
-for c in cnts:
-    area = cv2.contourArea(c)
-    if area > 1000:
-        x,y,w,h = cv2.boundingRect(c)
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0,0,0), 2)
-        # ROI = image[y:y+h, x:x+w]
-        # cv2.imwrite('ROI_{}.png'.format(ROI_number), ROI)
-        # ROI_number += 1
-
-
-cv2.imshow('image1', image)
-cv2.imwrite("FINAL.png",image)
-cv2.waitKey()
 cv2.destroyAllWindows()
